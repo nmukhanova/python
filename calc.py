9869978b6ac6bd6ef81c82802fcdb3eabe219941
Наталия Муханова <79748022+nmukhanova@users.noreply.github.com>
2021-03-26 03:17:56 +0300

    доработать

--- calc.py
+++ calc.py
@@ -1,44 +1,125 @@
-str_command = input("Please type command a + b or a-b: ")
-str_command.replace(' ','')
+str_command = input("Введите выражение:").replace(' ','')
+str_command = list(str_command)
 
-priority1 = tuple('^')
-priority2 = tuple('*/')
-priority3 = ('+', '-')
-operation_priority = priority1 + priority2 + priority3
-charts = tuple(map(str, range(10))) + tuple('.-')
+supported_ops = ('+-*/^sqrt')
+ops = {'+':2, '-':2, '/':1, '*':1, '^':0, 'sqrt':0}
 
-str_A = ''
-str_B = ''
 variables = ['']
 operations = []
+stack = []
+stroka = []
+output = []
+result = ''
+itog = []
+
 
 for i, letter in enumerate(str_command):
-	if letter in operation_priority and (i > 0) and variables[len(operations)] != '':
+	if letter in '+-/*^' and (i>0) and variables[len(operations)] != '':
 		operations.append(letter)
 		variables.append('')
 	else:
 		index = len(operations)
 		variables[index] = variables[index] + letter
 
+#разделение выражения на отдельные элементы
+for i in range(max(len(variables), len(operations))):
+	if i < len(variables):
+		stroka.append(variables[i])
+	if i < len(operations):
+		stroka.append(operations[i])
 
-variables = list(map(float, variables))
-result = variables[0]
 
-for i, operation in enumerate(operations):
-	if type(result) == str:
-		break
+#отрицательные числа
+for i, letter in enumerate(stroka):
+	if '-' in str(letter) and ('1' in str(letter) or '2' in str(letter) or '3' in str(letter) or '4' in str(letter) or '5' in str(letter) or '6' in str(letter) or '7' in str(letter) or '8' in str(letter) or '9' in str(letter)):
+		stroka[i] = str('(0' + stroka[i] + ')')	
 
-	var_A = result
-	var_B = variables[i+1]
+#sin
+for i, letter in enumerate(stroka):
+	if 'sin' in str(letter) and ('1' in str(letter) or '2' in str(letter) or '3' in str(letter) or '4' in str(letter) or '5' in str(letter) or '6' in str(letter) or '7' in str(letter) or '8' in str(letter) or '9' in str(letter)):
+		stroka[i] = str(math.sin((float(stroka[i].split('sin')).pop())))
 
-	if operation in '+-*/':
-		if var_B == 0 and operation == '/':
-			result = 'Inf'
+#cos
+for i, letter in enumerate(stroka):
+	if 'cos' in str(letter) and ('1' in str(letter) or '2' in str(letter) or '3' in str(letter) or '4' in str(letter) or '5' in str(letter) or '6' in str(letter) or '7' in str(letter) or '8' in str(letter) or '9' in str(letter)):
+		stroka[i] = str(math.cos((float(stroka[i].split('cos')).pop())))
+
+#sqrt
+for i, letter in enumerate(stroka):
+	if 'sqrt' in str(letter) and ('1' in str(letter) or '2' in str(letter) or '3' in str(letter) or '4' in str(letter) or '5' in str(letter) or '6' in str(letter) or '7' in str(letter) or '8' in str(letter) or '9' in str(letter)):
+		stroka[i] = str('(' + stroka[i] + '^0.5)')
+
+#обратная польская запись (вот тут не до конца разобралась, каюсь)
+str_command = ''.join(stroka)
+for i in str_command:
+	if i in '0123456789.':
+		if len(output) == 0:
+			output = [i] + output
 		else:
-			result = eval('{0}{1}{2}'.format(var_A, operation, var_B))
-	elif operation == '^':
-		result = var_A ** var_B
-	else:
-		result = "unknown"
+			if output[0][-1] in '0123456789.' and digit: output[0] += i
+			else:
+				output = [i] + output
+		digit = True
+	else: digit = False
 
-print("Result: " + str(result))
+	if i == '(':
+		stack = [i] + stack
+
+	if i == ')':
+		while stack != [] and stack[0] != '(': output, stack = [stack[0]] + output, stack[1:]
+		if stack != [] and stack[0] == '(': stack = stack[1:]
+
+	if i in ops:
+		while stack != [] and stack[0] in ops and ops[i] >= ops[stack[0]]: output, stack = [stack[0]] + output, stack[1:]
+		stack = [i] + stack
+
+while stack != []: output, stack = [stack[0]] + output, stack[1:]
+
+
+for i, letter in enumerate(output):
+	if '^' in str(letter) and '^' in str(output[i+2]):
+		output[i+1], output[i+2] = output[i+2], output[i+1]
+
+output = ''.join(reversed(output))
+
+#итог
+for i, letter in enumerate(output.split()):
+	if '.' in letter or '0' in letter or '1' in letter or '2' in letter or '3' in letter or '4' in letter or '5' in letter or '6' in letter or '7' in letter or '8' in letter or '9' in letter:
+		itog.append(letter)
+	elif letter == '^':
+		chislo1 = float(itog.pop())
+		chislo2 = float(itog.pop())
+		itog.append(chislo1**chislo2)
+	elif letter == 'sqrt':
+		chislo2 = float(itog.pop())
+		itog.append(chislo2**0.5)	
+	elif letter == '*':
+		chislo1 = float(itog.pop())
+		chislo2 = float(itog.pop())
+	elif letter == '/':
+		chislo1 = float(itog.pop())
+		chislo2 = float(itog.pop())
+		if chislo2 == 0:
+			result = 'Inf'
+			break
+		else:
+			itog.append(chislo1/chislo2)
+	elif letter == '+':
+		chislo1 = float(itog.pop())
+		chislo2 = float(itog.pop())
+		itog.append(chislo1+chislo2)
+	elif letter == '-':
+		chislo1 = float(itog.pop())
+		chislo2 = float(itog.pop())
+		itog.append(chislo1-chislo2)
+	else:
+		print('Ответ')
+
+if result == 'Inf':
+	print('Попробуйте ещё раз')
+else:
+	print('result:' + str(itog[0]))
+
+
+
+
