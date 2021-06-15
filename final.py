a0531ea4f0dec0e3a9c511b68ac7069bfdcc1639
Наталия Муханова <79748022+nmukhanova@users.noreply.github.com>
2021-06-15 14:06:55 +0300

    программа

---
+++ final.py
@@ -0,0 +1,120 @@
+#тема: Разработка методики расчёта эффективности маркетинговых акций АО «Международный аэропорт Сочи»
+
+from tkinter import *
+from tkinter import messagebox
+
+
+#главная страница: фоновое изображение + кнопка "Начать"
+def mainWindow1():
+    root = Tk()
+    root.resizable(width = False, height = False)
+    root.geometry('1440x900')
+    root.title("Расчёт эффективности маркетинговых акций")
+    root.iconbitmap( '/Users/user/Desktop/test/aircraft.ico' )
+    background = Canvas(root)
+    filename = PhotoImage(file = "/Users/user/Desktop/test/фон.png")
+    background_label = Label(root, image=filename)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+    background.pack()
+    def butCallback():
+        root.destroy()
+        mainWindow2()
+
+    but1 = Button(root, text = "Начать", font = ('Arial Bold', 20), command = butCallback )
+    but1.pack(padx = 612, pady = 223, side = 'bottom')
+    root.mainloop()
+
+#второе окно: ввод исходных данных
+def mainWindow2():
+    root = Tk()
+    #root.resizable(width = False, height = False)
+    root.geometry('1440x900')
+    root.title("Расчёт эффективности маркетинговых акций")
+    root.iconbitmap( '/Users/user/Desktop/test/aircraft.ico' )
+    root["bg"] = "white"
+
+    frame = Frame(root)
+    frame.pack()
+
+    group1 = LabelFrame(padx=15, pady=10, text="Введите исходные данные для расчёта", font = ('Arial Bold', 20))
+    group1.pack()
+
+    ml = Label(group1, text = 'Количество участников', font = ('Arial Bold', 20), fg = 'black')
+    ml.pack()
+    members = Entry(group1, font = ('Arial 20'), fg = 'black',bg = '#CEECF5', relief = 'raised', justify = 'left', show = '')
+    members.pack()
+
+    cl = Label(group1, text = 'Коэффициент конверсии', font = ('Arial Bold', 20), fg = 'black')
+    cl.pack()
+    conversion = Entry(group1, font = ('Arial 20'), fg = 'black',bg = '#CEECF5', relief = 'raised', justify = 'left', show = '')
+    conversion.pack()
+
+    bl = Label(group1, text = 'Средний чек', font = ('Arial Bold', 20), fg = 'black')
+    bl.pack()
+    bill = Entry(group1, font = ('Arial 20'), fg = 'black',bg = '#CEECF5', relief = 'raised', justify = 'left', show = '')
+    bill.pack()
+
+    tl = Label(group1, text = 'Коэффициент транзакции', font = ('Arial Bold', 20), fg = 'black')
+    tl.pack()
+    transaction = Entry(group1, font = ('Arial 20'), fg = 'black',bg = '#CEECF5', relief = 'raised', justify = 'left', show = '')
+    transaction.pack()
+
+
+    fcl = Label(group1, text = 'Постоянные затраты', font = ('Arial Bold', 20), fg = 'black')
+    fcl.pack()
+    fc = Entry(group1, font = ('Arial 20'), fg = 'black',bg = '#CEECF5', relief = 'raised', justify = 'left', show = '')
+    fc.pack()
+
+    vcl = Label(group1, text = 'Переменные затраты', font = ('Arial Bold', 20), fg = 'black') 
+    vcl.pack()
+    vc = Entry(group1, font = ('Arial 20'), fg = 'black',bg = '#CEECF5', relief = 'raised', justify = 'left', show = '')
+    vc.pack()
+
+    def cost_calculator():
+        cost = float(fc.get()) + float(vc.get())
+        print(cost)
+    button_calc1 = Button(root, text="Расчет затрат", command=cost_calculator, font = ('Arial Bold', 20))
+    button_calc1.pack()
+
+    def revenue_calculator():
+        revenue = float(members.get()) * float(bill.get())
+        print(revenue)
+    button_calc2 = Button(root, text="Расчет выручки", command=revenue_calculator, font = ('Arial Bold', 20))
+    button_calc2.pack()
+
+#здесь должна была быть одна кнопка "далее" и вывод расчетов в новом окне, но не получилось
+    def profit_calculator():
+        profit = float(members.get()) * float(conversion.get()) * float(bill.get()) * float(transaction.get())
+        print(profit)
+    button_calc3 = Button(root, text="Расчет прибыли", command=profit_calculator, font = ('Arial Bold', 20))
+    button_calc3.pack()
+
+    def bep_calculator():
+        bep = float(fc.get()) + float(vc.get())
+        print(bep)
+    button_calc4 = Button(root, text="Расчет точки безубыточности", command=bep_calculator, font = ('Arial Bold', 20))
+    button_calc4.pack()
+
+
+    def romi_calculator():
+        romi = (float(members.get()) * float(conversion.get()) * float(bill.get()) * float(transaction.get()) - (float(fc.get()) + float(vc.get()))) / (float(fc.get()) + float(vc.get())) * 100
+        print(romi)
+        if romi > 100:
+            messagebox.showinfo('OK', 'Акция эффективна')
+        elif romi < 100:
+            messagebox.showerror('Error', 'Акция неэффективна')
+    btn = Button(root, text="Расчет ROMI", command=romi_calculator, font = ('Arial Bold', 20))
+    btn.pack()
+   
+if __name__ == '__main__':
+    mainWindow1()
+
+'''
+    
+для справки
+cost(затраты) = fc + vc
+revenue(выручка) = members * bill
+profit(прибыль) = members * conversion * bill * transaction
+break_even_point(точка безубыточности) = fc + vc
+ROMI = ((revenue - cost)/cost) * 100
+'''
